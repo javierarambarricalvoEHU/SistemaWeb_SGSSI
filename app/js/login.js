36ca148ef3b85721283fc2a0ee2ec96f4b86bfc7
Javier Arambarri Calvo <javierarambarricalvo.ehu@gmail.com>
2023-09-18 22:47:33 +0200

    comprobación js nombre, apellido y teléfono comprobación número dni con letra (falta comproabar si el formato es correcto)

--- app/js/login.js
+++ app/js/login.js
@@ -1,10 +1,15 @@
 function checkName(){
     var name= document.signUp.name.value;
 
-    window.alert ("Su nombre: "+name); 
-    
-
-    
+    for (let i = 0; i < name.length; i++){
+        var ascii = name.charCodeAt(i);
+        if (ascii < 65 || ascii > 122){
+            window.alert ("Asegúrate de solo introducir texto: "+name);
+            return false;
+        }
+        
+    }
+    return true;
         
     /*if (edad < 18) {
             window.alert ("Lo siento. Debes ser mayor de edad"); 
@@ -16,8 +21,35 @@ function checkName(){
     /*more info at https://www.w3resource.com/javascript/form/all-letters-field.php#:~:text=To%20get%20a%20string%20contains,%2F)%20which%20allows%20only%20letters.*/
 }
 
-function checkDNI(){
+function checkSurname(){
+    var surname= document.signUp.surname.value;
 
+    for (let i = 0; i < surname.length; i++){
+        var ascii = surname.charCodeAt(i);
+        if (ascii < 65 || ascii > 122){
+            window.alert ("Asegúrate de solo introducir texto: "+surname);
+            return false;
+        }
+        
+    }
+    return true;
+}
+
+function checkDNI(){
+    //Falta comprobar que el formato del DNI introducido sea correcto
+    
+    var letras = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E','T'];
+    var dni=document.signUp.dni.value;
+    var letraInput=dni[9].toString().toUpperCase();
+    var dniSinLetra=dni.slice(0,8);
+
+    dniSinLetra=dniSinLetra%23;
+    
+    if (letras[dniSinLetra]!=letraInput){
+        window.alert("Error DNI: no corresponde el número con la letra");
+        return false;
+    }
+    return true;
 }
 
 function checkTel(){
@@ -30,13 +62,14 @@ function checkTel(){
            return false;
         }
      }
-     window.alert ("El número: "+tel+" es CORRECTO");
+
      return true;
     
 }
 
 function checkData(){
     checkName();
-    //checkDNI();
+    checkSurname();
+    checkDNI();
     checkTel();
 }
